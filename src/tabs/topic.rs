59174462152b3255d6f1d1ae67d6bb018989e624
wangrushen <wrs369@88.com>
2024-12-08 12:54:01 +0800

    err handle

--- src/tabs/topic.rs
+++ src/tabs/topic.rs
@@ -28,6 +28,7 @@ use rdkafka::{
     consumer::{BaseConsumer, Consumer},
     producer::FutureProducer,
 };
+use tokio::task::JoinHandle;
 
 pub struct TopicTab {
     pub topic_list: TopicList,
@@ -38,6 +39,8 @@ pub struct TopicTab {
 
     err: Arc<Mutex<Option<String>>>,
     err_time: Arc<Mutex<Option<SystemTime>>>,
+
+    receive_handle: Option<JoinHandle<()>>,
 }
 
 pub struct TopicList {
@@ -77,29 +80,24 @@ impl TopicTab {
 
             err: Arc::new(Mutex::new(None)),
             err_time: Arc::new(Mutex::new(None)),
+            receive_handle: None,
         }
     }
 
-    fn set_error(&mut self, error: String) {
-        let mut err = self.err.lock().unwrap();
-        *err = Some(error.clone());
-        let mut err_time = self.err_time.lock().unwrap();
-        *err_time = Some(SystemTime::now());
-        drop(err);
-        drop(err_time);
-
+    pub fn set_error(&mut self, error: String) {
         let err = self.err.clone();
         let err_time = self.err_time.clone();
         tokio::spawn(async move {
-            tokio::time::sleep(Duration::from_secs(5)).await;
-            let time = err_time.lock().unwrap();
-            if let Some(t) = *time {
-                if t.elapsed().unwrap() >= Duration::from_secs(5) {
-                    let mut err = err.lock().unwrap();
-                    *err = None;
-                }
-            }
+            set_error(error, err, err_time);
         });
+    }
+
+    pub fn set_topic_page(&mut self, page: TopicPage) {
+        if self.topic_page != TopicPage::MessagesRecv && self.receive_handle.is_some() {
+            let handle = self.receive_handle.take().unwrap();
+            handle.abort();
+        }
+        self.topic_page = page;
     }
 
     pub fn render(&mut self, area: Rect, buf: &mut Buffer) {
@@ -144,9 +142,8 @@ impl TopicTab {
 
         match self.topic_page {
             TopicPage::Normal | TopicPage::Info => self.render_topic_info(area, buf, topic),
-            TopicPage::Messages | TopicPage::MessagesRecv => {
-                self.render_topic_messages(area, buf, topic)
-            }
+            TopicPage::Messages => self.render_topic_messages(area, buf, topic),
+            TopicPage::MessagesRecv => self.render_topic_messages_recv(area, buf, topic),
             TopicPage::Send | TopicPage::SendEdit => self.render_topic_send(area, buf),
         }
     }
@@ -194,36 +191,39 @@ impl TopicTab {
         Widget::render(list, area, buf);
     }
 
-    fn render_topic_messages(&self, area: Rect, buf: &mut Buffer, topic: &KafkaTopic) {
-        let block = Block::new()
-            .title(Line::raw(format!("Messages for {}", topic.name)).centered())
-            .borders(Borders::ALL)
-            .border_set(symbols::border::ROUNDED)
-            .border_style(THEME.borders)
-            .padding(Padding::horizontal(1));
+    fn render_topic_messages_recv(&self, area: Rect, buf: &mut Buffer, topic: &KafkaTopic) {
+        let block = messages_block(topic);
 
-        let mut tip_message = "Enter to recv messages";
         let messages = self.messages.lock().unwrap();
         if !messages.is_empty() {
-            if messages.is_empty() {
-                tip_message = "No messages or recv failed";
-            } else {
-                let items: Vec<ListItem> = messages
-                    .iter()
-                    .map(|m| {
-                        ListItem::new(Text::from(format!("{}: {} {}", m.offset, m.key, m.payload)))
-                    })
-                    .collect();
-                let list = List::new(items).block(block);
-                Widget::render(list, area, buf);
-                drop(messages);
-                return;
-            }
+            let items: Vec<ListItem> = messages
+                .iter()
+                .map(|m| {
+                    ListItem::new(Text::from(format!("{}: {} {}", m.offset, m.key, m.payload)))
+                })
+                .collect();
+            let list = List::new(items).block(block);
+            Widget::render(list, area, buf);
+            drop(messages);
+            return;
         }
 
         drop(messages);
 
-        let text = Text::from(vec![Line::raw(tip_message)]).style(THEME.tip);
+        let text = Text::from(vec![Line::raw("No messages or recv failed")]).style(THEME.tip);
+
+        let center_area = center(
+            area,
+            Constraint::Length(text.width() as u16),
+            Constraint::Length(1),
+        );
+        block.render(area, buf);
+        Paragraph::new(text).render(center_area, buf);
+    }
+
+    fn render_topic_messages(&self, area: Rect, buf: &mut Buffer, topic: &KafkaTopic) {
+        let block = messages_block(topic);
+        let text = Text::from(vec![Line::raw("Enter to recv messages")]).style(THEME.tip);
 
         let center_area = center(
             area,
@@ -317,20 +317,22 @@ impl TopicTab {
         admin: &AdminClient<DefaultClientContext>,
     ) -> Result<Mode> {
         if self.topic_page == TopicPage::SendEdit {
-            self.topic_page = match self.send_form.handle_key_press(key, producer).await {
+            let page = match self.send_form.handle_key_press(key, producer).await {
                 Ok(page) => page,
                 Err(e) => {
                     self.set_error(e.to_string());
                     TopicPage::SendEdit
                 }
             };
+
+            self.set_topic_page(page);
             return Ok(Mode::Tab);
         }
 
         match key.code {
             KeyCode::Esc | KeyCode::Char('q') => match self.topic_page {
                 TopicPage::Normal => return Ok(Mode::TabChoose),
-                _ => self.topic_page = TopicPage::Normal,
+                _ => self.set_topic_page(TopicPage::Normal),
             },
             KeyCode::Char('r') => return Ok(Mode::Refresh),
             KeyCode::Char('g') | KeyCode::Home => self.select_first(),
@@ -344,17 +346,17 @@ impl TopicTab {
                 self.create_topic(admin).await;
             }
             KeyCode::Char('i') => match self.topic_page {
-                TopicPage::Send => self.topic_page = TopicPage::SendEdit,
+                TopicPage::Send => self.set_topic_page(TopicPage::SendEdit),
                 _ => {}
             },
             // KeyCode::Char('d') => self.delete_topic(producer),
             KeyCode::Enter => match self.topic_page {
-                TopicPage::Send => self.topic_page = TopicPage::SendEdit,
+                TopicPage::Send => self.set_topic_page(TopicPage::SendEdit),
                 TopicPage::Messages => {
                     let messages = Arc::clone(&self.messages);
                     let consumer = Arc::clone(&consumer);
                     let err = self.err.clone();
-                    tokio::spawn(async move {
+                    self.receive_handle = Some(tokio::spawn(async move {
                         if let Err(e) = recv_messages(messages, consumer).await {
                             let mut err = err.lock().unwrap();
                             *err = Some(e.to_string());
@@ -362,8 +364,8 @@ impl TopicTab {
                             let mut err = err.lock().unwrap();
                             *err = Some("Recv messages finished".to_string());
                         }
-                    });
-                    self.topic_page = TopicPage::MessagesRecv;
+                    }));
+                    self.set_topic_page(TopicPage::MessagesRecv);
                     return Ok(Mode::Tab);
                 }
                 _ => self.topic_detail(),
@@ -377,7 +379,7 @@ impl TopicTab {
     fn select_none(&mut self) {
         match self.topic_page {
             TopicPage::Normal => self.topic_list.state.select(None),
-            _ => self.topic_page = TopicPage::Normal,
+            _ => self.set_topic_page(TopicPage::Normal),
         }
     }
 
@@ -385,9 +387,9 @@ impl TopicTab {
         match self.topic_page {
             TopicPage::Normal => self.topic_list.state.select_next(),
 
-            TopicPage::Info => self.topic_page = TopicPage::Messages,
-            TopicPage::Messages | TopicPage::MessagesRecv => self.topic_page = TopicPage::Send,
-            TopicPage::Send | TopicPage::SendEdit => self.topic_page = TopicPage::Info,
+            TopicPage::Info => self.set_topic_page(TopicPage::Messages),
+            TopicPage::Messages | TopicPage::MessagesRecv => self.set_topic_page(TopicPage::Send),
+            TopicPage::Send | TopicPage::SendEdit => self.set_topic_page(TopicPage::Info),
         }
     }
 
@@ -396,8 +398,8 @@ impl TopicTab {
             TopicPage::Normal => self.topic_list.state.select_previous(),
 
             TopicPage::Info => self.topic_page = TopicPage::Send,
-            TopicPage::Messages | TopicPage::MessagesRecv => self.topic_page = TopicPage::Info,
-            TopicPage::Send | TopicPage::SendEdit => self.topic_page = TopicPage::Messages,
+            TopicPage::Messages | TopicPage::MessagesRecv => self.set_topic_page(TopicPage::Info),
+            TopicPage::Send | TopicPage::SendEdit => self.set_topic_page(TopicPage::Messages),
         }
     }
 
@@ -442,6 +444,37 @@ pub async fn recv_messages(
             None => {
                 continue;
             }
+        }
+    }
+}
+
+fn messages_block(topic: &KafkaTopic) -> Block {
+    Block::new()
+        .title(Line::raw(format!("Messages for {}", topic.name)).centered())
+        .borders(Borders::ALL)
+        .border_set(symbols::border::ROUNDED)
+        .border_style(THEME.borders)
+        .padding(Padding::horizontal(1))
+}
+
+fn set_error(
+    error: String,
+    err_arc: Arc<Mutex<Option<String>>>,
+    err_time_arc: Arc<Mutex<Option<SystemTime>>>,
+) {
+    let mut err = err_arc.lock().unwrap();
+    *err = Some(error.clone());
+    let mut err_time = err_time_arc.lock().unwrap();
+    *err_time = Some(SystemTime::now());
+    drop(err);
+    drop(err_time);
+
+    std::thread::sleep(Duration::from_secs(5));
+    let time = err_time_arc.lock().unwrap();
+    if let Some(t) = *time {
+        if t.elapsed().unwrap() >= Duration::from_secs(5) {
+            let mut err = err_arc.lock().unwrap();
+            *err = None;
         }
     }
 }
